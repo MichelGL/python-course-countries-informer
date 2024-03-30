4f684ba6acb0c9bfe567490c8514659d73f67520
Michel0303 <mmgladko@edu.hse.ru>
2024-03-30 13:28:15 +0500

    5. Добавлен метод API для получения новостей

--- src/news/clients/shemas.py
+++ src/news/clients/shemas.py
@@ -10,7 +10,21 @@ from pydantic import BaseModel
 class NewsItemDTO(BaseModel):
     """
     Модель данных для представления новости.
+
+    .. code-block::
+
+        NewsInfoDTO(
+            source="BBC News",
+            "author"="BBC News",
+            "title"="What does the King's diagnosis mean for William, Harry and the other royals?",
+            "description"="It's been a bleak midwinter for the Royal Family.
+            Will the King's health news help to bring them together?",
+            "url"="https://www.bbc.co.uk/news/uk-68211941",
+            "published_at"="2024-02-06T12:37:22.3818701Z",
+        )
     """
+
+
 
     source: str
     author: Optional[str]
